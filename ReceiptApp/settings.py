874e21a9977cc77934eb7a337d15344a97f12467
Ryszard <49765395+Ryszard-S@users.noreply.github.com>
2022-06-28 19:14:22 +0200

    .

--- ReceiptApp/settings.py
+++ ReceiptApp/settings.py
@@ -35,7 +35,7 @@ print('DEBUG= ', DEBUG)
 
 ALLOWED_HOSTS = ['.localhost', '127.0.0.1', 'rachunek.herokuapp.com']
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
