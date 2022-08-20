4d6375d746c5db92c5dc9ec32de12e4c9d55c07c
Ryszard <49765395+Ryszard-S@users.noreply.github.com>
2022-08-20 08:19:35 +0200

    refactor 2

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+    entry: './frontend/src/index.js',
+
+    output: {
+        path: path.resolve(__dirname, "./frontend/static/frontend"),
+        filename: "[name].js",
+      },
+
+    // plugins: [
+    //     new HTMLWebpackPlugin({
+    //         template: './frontend/src/index.html'
+    //     })
+    // ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                        }
+                }
+            }
+        ]
+    }
+
+}
