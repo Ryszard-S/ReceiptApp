4708d284a68c355a128880030f8e21a12563f05a
Ryszard <49765395+Ryszard-S@users.noreply.github.com>
2022-10-29 02:15:10 +0200

    updaate cors

--- ReceiptApp/settings.py
+++ ReceiptApp/settings.py
@@ -163,13 +163,19 @@ REST_FRAMEWORK = {
 # ]
 
 # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+# ]  # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+# CORS_ALLOWED_ORIGIN_REGEXES = [
+#     'http://localhost:3000',
+# ]
+
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-]  # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    'http://localhost:3000',
+    "https://receiptapp.onrender.com",
+    "http://localhost:8080",
+    "http://localhost:8000",
 ]
 
 APPEND_SLASH = True
